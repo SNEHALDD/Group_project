6b87da6c22a1987b90211e061c36e44f641c28e3
Zara Khan <zxkhan.99@gmail.com>
2022-10-29 14:20:46 -0500

    metadata for youtube videos -z

--- Youtube_Analysis/streamlit_test.py
+++ Youtube_Analysis/streamlit_test.py
@@ -1,2 +1,71 @@
-import streamlit as st
-st.title('How Many Bots in this Youtube Video?')
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stderr",
+     "output_type": "stream",
+     "text": [
+      "NumExpr defaulting to 4 threads.\n"
+     ]
+    }
+   ],
+   "source": [
+    "import streamlit as st\n",
+    "import pandas as pd\n",
+    "import numpy as np\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "<streamlit.DeltaGenerator.DeltaGenerator at 0x10f8ffd90>"
+      ]
+     },
+     "execution_count": 2,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "#add title\n",
+    "st.title('How Many Bots in this Youtube Video')"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3.7.13 ('mlenv')",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.13"
+  },
+  "orig_nbformat": 4,
+  "vscode": {
+   "interpreter": {
+    "hash": "d4f1b2b26d282679a37c22ffd0d805ae9e6e49f4c74aa42e69980383f9b2a98b"
+   }
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
